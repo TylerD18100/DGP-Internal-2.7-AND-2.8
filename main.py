851135b77a88d8dff69a22dab2424c1f778c6985
Jack <126205380+1820018200@users.noreply.github.com>
2023-03-22 23:16:50 +0000

    fix the code it runs now

--- main.py
+++ main.py
@@ -5,37 +5,57 @@
   Due: 6 April 2023
   
   TIP: Use assessment guide to help guide you through this Internal '''
-from datetime import date
-# used to create a custom window for age calculator
+# used to create a custom window for triangle calculator
 import tkinter as tk
-
+from math import sqrt
 
 #                      FUNCTION
-def get_age():
+
+
+
+
+
+def get_sidec():
     # gets the three entries
-    a= int(e_date.get())
-    b=int(e_month.get())
-    h=int(e_year.get())
+    a = int(e_sidea.get())
+    b =int(e_sideb.get())   
+    c = sqrt(a**2 + b**2)
 
-def find_age(d, m, y):    
-    # find the age ( difference between current and date of birth )
-    age = today.year-y-((today.month, today.day)<(m,d))
-    return age
+    tbox_c.config(state='normal')
+  
+    #age calculated is inserted into the text box after clearing the previous info in the text box
+    
+    tbox_c.delete('1.0', tk.END)
+    tbox_c.insert(tk.END,c)
+    tbox_c.config(state='disabled')
+  
+def display_calc_c():    
+    tbox_c.config(state='normal')
 
-def display_calc_age(age):    
-    tbox_age.config(state='normal')
+    
+    #c calculated is inserted into the text box after clearing the previous info in the textbox.
+
+def display_calc_sidec(sidec):    
+    tbox_c.config(state='normal')
    
-    #age calculated is inserted into the text box after clearing the previous info in the textbox.
-    tbox_age.delete('1.0', tk.END)
-    tbox_age.insert(tk.END,age)
-    tbox_age.config(state='disabled')
+    print("Input lengths of shorter triangle sides:")
+    a = float(input("a: "))
+    b = float(input("b: "))
+    global c
+    c = sqrt(a**2 + b**2)
+
+    print("The length of the hypotenuse is:", c )
 
 def exit():
     window.destroy()
 
-#                      MAIN
+
+  
+  
+  
+#                      WINDOW MAIN
 # Create a object which stores today's whole date using datetime function
-today = date.today()
+
 
 # Creating a custom window
 
@@ -47,38 +67,37 @@ window.title('Right Angle triangle solver')
 
 #Labels for Heading and Subheading of GUI
 lb_heading = tk.Label(window,text="Right Angle Triangle Solver",font=("Arial",20),fg="black",bg="#F7DC6F")
-lb_subheading = tk.Label(window,font=("Arial",12),text="Enter your Triangle Side Values!",fg="black",bg="#F7DC6F")
+lb_subheading = tk.Label(window,font=("Arial",12),text="Enter your Triangle Side Values for Solving Hypotenuse Side!",fg="black",bg="#F7DC6F")
 
 # labels for date, month and year
-lb_date = tk.Label(window,text = "Leg A: ",font=('Arial',12,"bold"),fg="darkgreen",bg="#F7DC6F")
-lb_month = tk.Label(window,text = "Leg B: ",font=('Arial',12,"bold"),fg="darkgreen",bg="#F7DC6F")
-lb_year = tk.Label(window,text = "Hypotenuse: ",font=('Arial',12,"bold"),fg="darkgreen",bg="#F7DC6F")
+lb_sidea = tk.Label(window,text = "Side A: ",font=('Arial',12,"bold"),fg="darkgreen",bg="#F7DC6F")
+
+lb_sideb = tk.Label(window,text = "Side B: ",font=('Arial',12,"bold"),fg="darkgreen",bg="#F7DC6F")
+
 
 # Entry boxes for date, month and year
-e_date = tk.Entry(window,width=5)
-e_month = tk.Entry(window,width=5)
-e_hypotenuse = tk.Entry(window,width=5)
+e_sidea = tk.Entry(window,width=5)
+e_sideb = tk.Entry(window,width=5)
+#e_sidec = tk.Entry(window,width=5)
 
-# Button to calculate age
-btn_calculate_age = tk.Button(window,text="Solve",font=("Arial",13), command=get_age)
+# Button to solve triangle
+btn_calculate_sidec = tk.Button(window,text="Solve",font=("Arial",13), command=get_sidec)
 
 
-lb_calculated_age = tk.Label(window,text="The Solved Triangle",font=("Arial",12,"bold"),fg="darkgreen",bg="#F7DC6F") 
-tbox_age=tk.Text(window,width=5,height=0,state="disabled")
+lb_calculated_sidec = tk.Label(window,text="Side C = ",font=("Arial",12,"bold"),fg="darkgreen",bg="#F7DC6F") 
+tbox_c=tk.Text(window,width=5,height=0,state="disabled")
 
 
 btn_exit = tk.Button(window,text="Exit Application!",font=("Arial",13),command=exit)
 
 
 lb_heading.place(x=110,y=5)
-lb_subheading.place(x=135,y=40)
-lb_date.place(x=100,y=70)
-lb_month.place(x=100,y=95)
-lb_year.place(x=100,y=120)
-e_date.place(x=240,y=70)
-e_month.place(x=240,y=95)
-e_hypotenuse.place(x=240,y=120)
-btn_calculate_age.place(x=140,y=150)
-lb_calculated_age.place(x=50,y=200)
-tbox_age.place(x=240,y=203)
-btn_exit.place(x=100,y=230)
+lb_subheading.place(x=80,y=40)
+lb_sidea.place(x=160,y=90)
+lb_sideb.place(x=160,y=120)
+e_sidea.place(x=240,y=90)
+e_sideb.place(x=240,y=120)
+btn_calculate_sidec.place(x=180,y=150)
+lb_calculated_sidec.place(x=160,y=200)
+tbox_c.place(x=240,y=203)
+btn_exit.place(x=115,y=230)
